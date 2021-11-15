f6b7fe052c302b14474a374acb8d704d1e6333e9
banesullivan <bane.sullivan@kitware.com>
2021-11-15 13:21:46 -0700

    Bump RGD

--- opengeo/settings.py
+++ opengeo/settings.py
@@ -1,63 +1,26 @@
-import os
 from pathlib import Path
 from typing import Type
 
 from composed_configuration import (
     ComposedConfiguration,
     ConfigMixin,
+    CorsMixin,
     DevelopmentBaseConfiguration,
     HerokuProductionBaseConfiguration,
     ProductionBaseConfiguration,
     TestingBaseConfiguration,
 )
 from configurations import values
+from rgd.configuration import ResonantGeoDataBaseMixin
 
 
-class GeoDjangoMixin(ConfigMixin):
-    @staticmethod
-    def before_binding(configuration: Type[ComposedConfiguration]):
-        configuration.INSTALLED_APPS += ['django.contrib.gis']
-        try:
-            import re
-
-            import osgeo
-
-            libsdir = os.path.join(
-                os.path.dirname(os.path.dirname(osgeo._gdal.__file__)), 'GDAL.libs'
-            )
-            libs = {
-                re.split(r'-|\.', name)[0]: os.path.join(libsdir, name)
-                for name in os.listdir(libsdir)
-            }
-            configuration.GDAL_LIBRARY_PATH = libs['libgdal']
-            configuration.GEOS_LIBRARY_PATH = libs['libgeos_c']
-        except Exception:
-            # TODO: Log that we aren't using the expected GDAL wheel?
-            pass
-
-
-class SwaggerMixin(ConfigMixin):
-    REFETCH_SCHEMA_WITH_AUTH = True
-    REFETCH_SCHEMA_ON_LOGOUT = True
-    OPERATIONS_SORTER = 'alpha'
-    DEEP_LINKING = True
-
-
-class CrispyFormsMixin(ConfigMixin):
-    @staticmethod
-    def before_binding(configuration: Type[ComposedConfiguration]):
-        configuration.INSTALLED_APPS += ['crispy_forms']
-
-
-class OpenGeoMixin(CrispyFormsMixin, GeoDjangoMixin, SwaggerMixin, ConfigMixin):
+class OpenGeoMixin(ResonantGeoDataBaseMixin, CorsMixin, ConfigMixin):
     WSGI_APPLICATION = 'opengeo.wsgi.application'
     ROOT_URLCONF = 'opengeo.urls'
 
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-    # A setting to allow read access to all users if permissions are unassigned
-    RGD_GLOBAL_READ_ACCESS = values.Value(default=False)
-
+    # Override to allow anyone to sign up
     RGD_AUTO_APPROVE_SIGN_UP = values.Value(default=True)
 
     @staticmethod
@@ -81,28 +44,8 @@ class OpenGeoMixin(CrispyFormsMixin, GeoDjangoMixin, SwaggerMixin, ConfigMixin):
             'rgd_imagery',
         ]
 
-        configuration.REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'].append(
-            'rest_framework.authentication.BasicAuthentication'
-        )
-
-        configuration.AUTHENTICATION_BACKENDS.insert(0, 'rules.permissions.ObjectPermissionBackend')
-
-        configuration.MIDDLEWARE += ('crum.CurrentRequestUserMiddleware',)
-
-    # This cannot have a default value, since the password and database name are always
-    # set by the service admin
-    DATABASES = values.DatabaseURLValue(
-        environ_name='DATABASE_URL',
-        environ_prefix='DJANGO',
-        environ_required=True,
-        # Additional kwargs to DatabaseURLValue are passed to dj-database-url
-        engine='django.contrib.gis.db.backends.postgis',
-        conn_max_age=600,
-    )
-
-    CELERY_WORKER_SEND_TASK_EVENTS = True
-
-    RGD_FILE_FIELD_PREFIX = values.Value(default=None)
+    # To use endpoints from external origin
+    CORS_ORIGIN_ALLOW_ALL = True
 
 
 class DevelopmentConfiguration(OpenGeoMixin, DevelopmentBaseConfiguration):
