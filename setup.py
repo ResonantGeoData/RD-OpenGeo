ea88c418490f14e98d7820958fbbfe25a6612805
Bane Sullivan <banesullivan@gmail.com>
2022-01-20 13:19:04 -0700

    Bump django-rgd-3d, django-rgd-fmv, django-rgd-geometry and django-rgd-imagery
    Bumps [django-rgd-3d](https://github.com/ResonantGeoData/ResonantGeoData), [django-rgd-fmv](https://github.com/ResonantGeoData/ResonantGeoData), [django-rgd-geometry](https://github.com/ResonantGeoData/ResonantGeoData) and [django-rgd-imagery](https://github.com/ResonantGeoData/ResonantGeoData). These dependencies needed to be updated together.
    
    Updates `django-rgd-3d` from 0.2.13 to 0.2.15
    - [Release notes](https://github.com/ResonantGeoData/ResonantGeoData/releases)
    - [Commits](https://github.com/ResonantGeoData/ResonantGeoData/compare/0.2.13...0.2.15)
    
    Updates `django-rgd-fmv` from 0.2.13 to 0.2.15
    - [Release notes](https://github.com/ResonantGeoData/ResonantGeoData/releases)
    - [Commits](https://github.com/ResonantGeoData/ResonantGeoData/compare/0.2.13...0.2.15)
    
    Updates `django-rgd-geometry` from 0.2.13 to 0.2.15
    - [Release notes](https://github.com/ResonantGeoData/ResonantGeoData/releases)
    - [Commits](https://github.com/ResonantGeoData/ResonantGeoData/compare/0.2.13...0.2.15)
    
    Updates `django-rgd-imagery` from 0.2.13 to 0.2.15
    - [Release notes](https://github.com/ResonantGeoData/ResonantGeoData/releases)
    - [Commits](https://github.com/ResonantGeoData/ResonantGeoData/compare/0.2.13...0.2.15)

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'django-rgd-3d==0.2.13',
         'django-rgd-fmv==0.2.13',
         'django-rgd-geometry==0.2.13',
-        'django-rgd-imagery==0.2.13',
+        'django-rgd-imagery==0.2.15',
     ],
     extras_require={
         'dev': [
